2b2a9f59790e0c1c406f6878f44fa1fecd1847b5
TheLordPasta <arielshirkanistudent@gmail.com>
2024-01-28 22:05:06 +0200

    added dash and jumps only ones
    need to study about the lerping and acceleration, and fucking ofek needs to give me the fucking tomato!

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
 const MAX_SPEED = 280	
-const ACCELERATION_SMOOTHING = 12
-const  GRAVITY = 10
-const  JUMP_POWER = 300
+const ACCELERATION_SMOOTHING_FOR_MOVEMENT = 10
+const ACCELERATION_SMOOTHING_FOR_DASH = 30
+const  GRAVITY = 40
+const  JUMP_POWER = 900
+const DASH_POWER = 15000
 
 @onready var collition_shape = $CollisionShape3D
 @onready var health_component = $HealthComponent
@@ -22,13 +24,14 @@ func _process(delta):
 	var direction = momvement_vector.normalized()
 	var target_velocity = direction * MAX_SPEED 
 	
-	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
+	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING_FOR_MOVEMENT))
 	
 	velocity.y -= GRAVITY
 	if is_on_floor() == true:
 		velocity.y = 0
 	
 	jump()
+	dash(delta)
 	move_and_slide()
 	
 
@@ -38,8 +41,25 @@ func get_movement_vector():
 	return Vector3(x_movement, 0, 0)
 
 
-func jump():
-	if Input.is_action_just_pressed("jump"):
+func jump(): 
+	if Input.is_action_just_pressed("jump") and velocity.y == 0:
 		velocity.y = JUMP_POWER
 		print("just jumped")
+
+
+func dash(delta):
+	var dash_direction
+	if Input.is_action_just_pressed("dash") and velocity.y == 0:
+		if Input.is_action_pressed("move_right"):
+			dash_direction = Vector3.LEFT
+	
+		elif Input.is_action_pressed("move_left"):
+			dash_direction = Vector3.RIGHT
 		
+		velocity = velocity.lerp(dash_direction * JUMP_POWER, - exp(-delta * ACCELERATION_SMOOTHING_FOR_DASH))
+		print("just dashed")
+		print(velocity)
+		
+
+
+	
